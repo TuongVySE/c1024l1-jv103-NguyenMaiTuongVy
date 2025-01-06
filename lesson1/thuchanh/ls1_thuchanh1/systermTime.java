40a61eae0ac4565b66276d397f0aeedcefeb0575
Vynmt198 <Vynmtse181954@fpt.edu.vn>
2025-01-06 13:05:12 +0700

    lesson1_thuchanh1

---
+++ lesson1/thuchanh/ls1_thuchanh1/systermTime.java
@@ -0,0 +1,9 @@
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+public class systermTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
